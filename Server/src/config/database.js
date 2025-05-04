a8cf4ca544db71a507b421b753e9b376e6923681
Sumea <sumeaqa@gmail.com>
2025-05-04 21:29:50 +0200

    Database config

--- Server/src/config/database.js
+++ Server/src/config/database.js
@@ -1,1 +1,12 @@
 //Lidhjen me databaze
+
+const { Sequelize } = require('sequelize');
+
+// Set up the connection to the database
+const sequelize = new Sequelize('users', 'root', 'coraline', {
+  host: 'localhost',
+  dialect: 'mysql',
+  logging: true, 
+});
+
+module.exports = sequelize;
