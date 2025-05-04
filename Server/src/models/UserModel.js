50a3a28cbb2bf8c7929bd0ef6568ce34eab780dc
Sumea <sumeaqa@gmail.com>
2025-05-04 21:30:28 +0200

    Defined user models

--- Server/src/models/UserModel.js
+++ Server/src/models/UserModel.js
@@ -1,1 +1,44 @@
 //E bon definimin e tabeles User per Databaz
+
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database'); 
+
+// Define the User model
+const User = sequelize.define('User', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  role: {
+    type: DataTypes.ENUM('admin', 'user', 'moderator'),
+    defaultValue: 'user',
+  },
+  createdAt: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW,
+  },
+  updatedAt: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW,
+    onUpdate: DataTypes.NOW,
+  },
+}, {
+  tableName: 'Users',
+});
+
+module.exports = User;
